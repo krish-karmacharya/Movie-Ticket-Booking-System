584e74e99bb833d90fd7065960bfe1e5a0ff9f19
Krish <karmacharya.krish1@gmail.com>
2024-09-12 13:00:49 +0545

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import StreamingSection from "./components/StreamingSection";
-
+import StreamingSection from "./pages/StreamingSection";
+import LoginPage from "./pages/Loginpage";
+import UpComming from "./pages/UpComming"
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      {/* <LoginPage /> */}
       <Header />
       <StreamingSection />
+      {/* <UpComming /> */}
       <Footer />
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,10 @@ const Header = () => {
     <nav className="bg-[#1E2028] text-white p-3">
       {/* Desktop menu */}
       <div className="hidden md:flex justify-between items-center container mx-auto px-4 py-2">
-        <a href="#" className="font-bold text-2xl text-white bg-blue-600 px-3 py-1 rounded">
+        <a
+          href="#"
+          className="font-bold text-2xl text-white bg-blue-600 px-3 py-1 rounded"
+        >
           Logo
         </a>
         <ul className="flex space-x-6">
@@ -27,7 +30,7 @@ const Header = () => {
             </a>
           </li>
           <li>
-            <a href="upcomming_movies.html" className="hover:text-gray-300 text-lg">
+            <a href="#" className="hover:text-gray-300 text-lg">
               Upcoming
             </a>
           </li>
@@ -37,12 +40,12 @@ const Header = () => {
             </a>
           </li>
           <li>
-            <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-base px-5 py-2 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2">
-              <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-                <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd" />
-              </svg>
-              Sign in with Google
-            </button>
+            <div className="flex items-center justify-center">
+              <div className="border w-fit rounded-xl shadow-sm">
+                <button className="px-4 py-2 rounded-l-xl text-white m-0 bg-red-500 hover:bg-red-600 transition">Login</button>
+                <button className="px-4 py-2 rounded-r-xl bg-neutral-50 hover:bg-neutral-100 transition text-black hover:text-gray-700">Register</button>
+              </div>
+            </div>
           </li>
         </ul>
       </div>
@@ -107,12 +110,12 @@ const Header = () => {
                     </a>
                   </li>
                   <li className="px-6 py-2">
-                    <button type="button" className="w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-base px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55">
-                      <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-                        <path fillRule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clipRule="evenodd" />
-                      </svg>
-                      Sign in with Google
-                    </button>
+                    <div className="flex items-center justify-center">
+                      <div className="border w-full rounded-xl shadow-sm">
+                        <button className="px-4 py-2 rounded-l-xl text-white m-0 bg-red-500 hover:bg-red-600 transition">Login</button>
+                        <button className="px-4 py-2 rounded-r-xl bg-neutral-50 hover:bg-neutral-100 transition text-black hover:text-gray-700">Register</button>
+                      </div>
+                    </div>
                   </li>
                 </ul>
               </nav>
